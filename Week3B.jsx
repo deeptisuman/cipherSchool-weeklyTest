4b5563f6dd1560631a0598467b7181b2ceaaf71d
deeptisuman <81811138+deeptisuman@users.noreply.github.com>
2023-07-08 23:16:28 +0530

    Create Week3B.jsx

---
+++ Week3B.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Screen</h1>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
